03c56a24d217d1872753a22716f6d77d0763ba9b
CarlosEdDev <CarlosEdsdeJesus@gmail.com>
2023-09-05 06:41:46 -0300

    Acionando o codigo novamente

--- collections-java-api-2023/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.collectionsjavaapi2023 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.collectionsjavaapi2023 to javafx.fxml;
-    exports com.example.collectionsjavaapi2023;
-}
